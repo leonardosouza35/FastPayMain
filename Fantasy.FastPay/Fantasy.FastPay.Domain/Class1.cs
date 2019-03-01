313a78c529d5e7fc41f72ecc57726d4b3fc512fd
leonardosouza35 <leonardosouza35@outlook.com>
2019-03-01 09:39:13 -0300

    Create Class1.cs
    nova classe adicionada

---
+++ Fantasy.FastPay/Fantasy.FastPay.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fantasy.FastPay.Domain
+{
+    class Class1
+    {
+        public int MyProperty { get; set; }
+    }
+}
