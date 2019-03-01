f371fd72f59e4a23bb49812b8a84d3116a1563b5
IPeixoto <snavisp@gmail.com>
2019-03-01 10:15:42 -0300

    Create Pessoa.cs
    Criação de nova classe na branch master

---
+++ Fantasy.FastPay/Fantasy.FastPay.Domain/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fantasy.FastPay.Domain
+{
+    class Pessoa
+    {
+        public int MyProperty { get; set; }
+    }
+}
