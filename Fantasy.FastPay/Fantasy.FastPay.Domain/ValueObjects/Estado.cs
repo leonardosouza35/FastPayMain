c25dffd019e72f67d240868d1849306da12d1499
leonardosouza35 <leonardosouza35@outlook.com>
2019-03-22 09:41:06 -0300

    Script criação da base de dados

---
+++ Fantasy.FastPay/Fantasy.FastPay.Domain/ValueObjects/Estado.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fantasy.FastPay.Domain.ValueObjects
+{
+    public class Estado : ValueObjet
+    {        
+                
+    }
+}
--- Fantasy.FastPay/Fantasy.FastPay.Infra.Data/Scripts/CriacaoTabelas.sql
+++ Fantasy.FastPay/Fantasy.FastPay.Infra.Data/Scripts/CriacaoTabelas.sql
@@ -6,31 +6,128 @@ create table Paises
 (
    Id int not null identity primary key,
    Nome varchar(50) not null,
-   DataCadastro DateTime 
+   Descricao varchar(50) not null,
+   DataCadastro datetime not null default CURRENT_TIMESTAMP      
 )
 
-
-create table Paises
+create table Estados
 (
    Id int not null identity primary key,
    Nome varchar(50) not null,
-   DataCadastro DateTime 
+   Descricao varchar(50) not null
 )
+
+create table Cidades
+(
+   Id int not null identity primary key,
+   Nome varchar(50) not null,
+   Descricao varchar(50) not null,
+   EstadoId int not null references Estados(Id)
+)
+
+create table TipoPessoa(
+   Id int not null identity primary key,
+   Nome varchar(50) not null,
+   Descricao varchar(50) not null   
+)
+
+create table Endereco(
+	CEP varchar(50) not null primary key,
+	Nome varchar(50) not null,
+	Bairro varchar(50) not null,
+	LocalidadeUF varchar(50) not null,
+)
+
+
+create table TipoConta
+(
+	Id int not null identity primary key,
+	Nome varchar(50) not null,
+	Descricao varchar(50) not null   
+)
+
+create table Usuarios(
+	Id int not null identity primary key,
+	Nome varchar(50) not null,
+	SobreNome varchar(50) not null,
+	Senha varchar(100) not null,
+	TipoPessoaId int not null references TipoPessoa(Id),
+	NumeroDocumento varchar(50) not null,
+	NomeEmpresa varchar(50) not null,
+	CEP varchar(50) not null references Endereco(CEP),	
+	Telefone1 varchar(50) not null,
+	Telefone2 varchar(50) not null,
+	CodigoBanco int not null,
+	NomeBanco varchar(50) not null,
+	Agencia varchar(50) not null,
+	TipoDeContaId int not null references TipoConta(Id),
+	NumeroDaConta varchar(50) not null
+)
+
+
+create table BancoPagador
+(
+   Id int not null identity primary key,
+   Nome varchar(50) not null,
+   DataCadastro datetime not null default CURRENT_TIMESTAMP      
+)
+
 
 create table Empresas
 (
 	Id int not null identity primary key,
+	DataCadastro datetime not null default CURRENT_TIMESTAMP,
 	Nome varchar(50) not null,
-	PaisOrigemId int not null,
-	EstadoId int not null,
-	CidadeId int not null,
-	PercentualCalculoTaxa money null
-	Foreign Key(PaisOrigemId) references(Paises)
+	PaisOrigemId int not null references Paises(Id),
+	EstadoId int not null references Estados(Id),
+	CidadeId int not null references Cidades(Id),
+	PercentualCalculoTaxa decimal(18,10) not null		
 );
 
-insert into FastPayDB.dbo.Empresas(Nome, PaisOrigemId,EstadoId,CidadeId,PercentualCalculoTaxa)
-values('São Paulo',1,1,1,570.67);
+create table OpcaoMoeda
+(
+	Id int not null identity primary key,
+	Nome varchar(50) not null,
+	Descricao varchar(50) not null   
+)
 
-select * from dbo.Empresas;
+create table StatusOrdem
+(
+	Id int not null identity primary key,
+	Nome varchar(50) not null,
+	Descricao varchar(50) not null   
+)
+
+
+
+create table OrdemPagamento(
+	Id int not null identity primary key,
+	DataCadastro datetime not null default CURRENT_TIMESTAMP,	
+	DataEnvio datetime not null default CURRENT_TIMESTAMP,
+	DataPagamentoRecebido datetime not null default CURRENT_TIMESTAMP,
+	UsuarioQueEnviouId int not null references Usuarios(Id),  
+	UsuarioQueRecebeuId int not null references Usuarios(Id),  
+	EmpresaId int not null references Empresas(Id),  
+	BancoPagadorId int not null references BancoPagador(Id),  
+	OpcaoMoedaOrigem int not null references OpcaoMoeda(Id),  
+	OpcaoMoedaDestino int not null references OpcaoMoeda(Id),  
+	ValorEnviado money not null,
+	ValorRecebido money not null,
+	TaxaCambioMercado decimal(18,10) not null,
+	TaxaCambioEmpresa decimal(18,10) not null,
+	StatusOrdemId int not null references StatusOrdem(Id)	
+)
+
+create table OrdemPagamentoPasso
+(
+   Id int not null identity primary key,
+   DataPasso datetime not null default CURRENT_TIMESTAMP,
+   StatusOrdemId int not null,
+   Descricao varchar(4000) not null,
+   OrdemPagamentoId int not null references OrdemPagamento(Id),
+   UsuarioId int not null references Usuarios(Id),      
+)
+
+
 
 
