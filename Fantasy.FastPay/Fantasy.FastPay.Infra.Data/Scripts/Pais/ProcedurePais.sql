6682fb21f3180e10f86689c12f7644ddc1488d5f
leonardosouza35 <leonardosouza35@outlook.com>
2019-03-25 10:33:46 -0300

    Update ProcedurePais.sql

--- Fantasy.FastPay/Fantasy.FastPay.Infra.Data/Scripts/Pais/ProcedurePais.sql
+++ Fantasy.FastPay/Fantasy.FastPay.Infra.Data/Scripts/Pais/ProcedurePais.sql
@@ -1,12 +1,22 @@
 use [FastPayDB]
 go
 
+--ADICIONAR PAIS 
 CREATE Procedure [dbo].[spAdicionarPais] 
 
 	@Nome	varchar(50),	
 	@Descricao	varchar(50)			
 AS
 	INSERT INTO Paises(Nome, Descricao, DataCadastro) values(@Nome,@Descricao, CURRENT_TIMESTAMP);
+	                                     
+	RETURN;
+
+
+--ADICIONAR PAIS 
+CREATE Procedure [dbo].[spObterTodosPaises] 
+	
+AS
+	select nome, descricao, datacadastro from Paises;
 	                                     
 	RETURN;
 
